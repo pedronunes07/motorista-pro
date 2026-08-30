fe95b10f4b26ef5b7935896f129a10ff7cd08c00
pedronunes07 <148357835+pedronunes07@users.noreply.github.com>
2026-08-30 11:12:50 -0300

    Create widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:motorista_pro/main.dart';
+
+void main() {
+  test('aplicativo pode ser criado', () {
+    expect(const MotoristaProApp(), isA<MotoristaProApp>());
+  });
+}
