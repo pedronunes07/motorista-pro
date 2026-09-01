1d58c2a1cece866caf334359f2805754e1fc714c
pedronunes07 <148357835+pedronunes07@users.noreply.github.com>
2026-08-31 21:31:45 -0300

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+android {
+    namespace = "com.motoristapro.app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
+    defaultConfig {
+        applicationId = "com.motoristapro.app"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+    buildTypes { release { signingConfig = signingConfigs.getByName("debug") } }
+}
+flutter { source = "../.." }
+
+dependencies {
+    implementation("com.google.mlkit:text-recognition:16.0.1")
+}
